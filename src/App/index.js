7b825674cdf68e13ee8b6c2174f38092be354f0a
oneil <oneilcampbell19@gmail.com>
2019-06-13 22:40:36 -0400

    changed to functional component to simplify logic

--- src/App/index.js
+++ src/App/index.js
@@ -1,45 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from '../Components/Login';
+import Signup from '../Components/Signup';
 import Profile from '../Components/Profile';
 import Timeline from '../Components/Timeline';
 import "./style.css";
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      userInfo:{},
-      profileProps:null
-    }
-  }
-
-  getProfileProps = (profileProps) => {
-    this.setState({profileProps});
-  }
-
-  render() {
-     return (this.state.profileProps === null ? 
-        (
-        <div className="App">
-          <Switch>
-            <Route exact path="/" render={() => <Login getProfileProps={this.getProfileProps} />} />
-             <Route path="/users" component= {Profile} />
-            <Route path="/timeline" component={Timeline} />
-          </Switch>
-        </div>
-        ) :
-        (
-         <div className="App">
-           <Switch>
-             <Route exact path="/" render={() => <Profile {...this.state.profileProps} />} />
-             <Route path={`/users/${this.state.profileProps.id}`} render={() => <Profile {...this.state.profileProps} />} />
-             <Route path="/timeline" component={Timeline} />
-           </Switch>
-         </div>
-        )
-
-      )
-  }
+const App = () => {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/users" component= {Profile} />
+          <Route path="/timeline" component={Timeline} />
+        </Switch>
+      </div>
+    )
 }
+
 export default App;
