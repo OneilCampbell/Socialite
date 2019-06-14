54d15fadf6223f401073da4719263184011fcacb
oneil <oneilcampbell19@gmail.com>
2019-06-14 00:02:28 -0400

    decluttered profile page

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import Posts from '../Posts'
+import Posts from '../Posts';
+import Header from '../Header';
 import axios from "axios";
 import "./style.css";
 import mp3_file from "../../Assets/Humble.mp3";
@@ -26,15 +27,15 @@ class Profile extends Component {
    handleCreatePost = async () =>{
       const createdPost ={
           content: this.state.content,
-          image: 'https://wallpaperstock.net/wallpapers/thumbs1/40737wide.jpg',
-          userId:this.props.id
+          image: '',
+          userId:this.props.location.state.id
       }
       await axios.post('/posts',createdPost)
       .then( () => this.getUsersPostsData());
   }
 
    getUsersPostsData = async() => {
-      await axios.get(`/posts/${this.props.id}`)
+      await axios.get(`/posts/${this.props.location.state.id}`)
       .then(response => {
          const usersPosts = response.data;
          this.setState({ usersPosts, create: false });
@@ -47,15 +48,16 @@ class Profile extends Component {
 
    render() {
       const usersPosts = this.state.usersPosts;
-      const allTheirPosts = usersPosts === null ? usersPosts : usersPosts.map(post => <Posts key={post.id} reRender={this.getUsersPostsData} postInfo={{ ...post }} userInfo={{ ...this.props }} />)
-      const userInfo = this.props;
+      const allTheirPosts = usersPosts === null ? usersPosts : usersPosts.map(post => <Posts key={post.id} reRender={this.getUsersPostsData} postInfo={{ ...post }} currentUserInfo={{ ...this.props.location.state }} />)
+      const userInfo = this.props.location.state;
       const create = this.state.create
       return (
          <div  className="profile-page">
+            <Header userInfo={userInfo} comingFrom={"profile"}/>
             <div className="center">
                <div className="card info-card">
                      <div className="card-image">
-                        <img src={userInfo.image}  />
+                        <img src={userInfo.image || "http://www.3ccc.co.nz/wp-content/uploads/2015/02/Profile-Placeholder.png"}  alt="profile avatar"/>
                      </div>
                   <div className="card-content">
                      <div className="media">
@@ -63,18 +65,13 @@ class Profile extends Component {
                         </div>
                         <div className="media-content">
                            <p className="users-name subtitle is-6">{`${userInfo.firstname} ${userInfo.lastname}`}</p>
-                           <span>
+                           <div className="users-handle-email">
                               <p className="handle"> @{userInfo.username}</p>
-                              <p className="email">{userInfo.email}</p>
-                           </span>
+                              <p className="email">{userInfo.email || `${userInfo.username}@socialite.com`}</p>
+                           </div>
                            <br />
                            <br />
                         </div>
-                     </div>
-                     <div className="content">
-                        <a>@ socialite</a>.
-                        <a href="#">#👑</a> <a href="#">#tech-enthusiast</a>
-                        <p className="tagline">they believed they could so they did</p>
                      </div>
                   </div>
                </div>
